f261e8640a5ff95eeffee6176d26097583c43ccf
Miguel Santiago Gómez Suárez <Hitomiblood@gmail.com>
2025-02-02 21:06:11 -0500

    contraseña a proposito subida

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Probando Commit User!");
+
+string stripeApiKey = "12345abcde67890fghij12345klmno67890pqrst";
