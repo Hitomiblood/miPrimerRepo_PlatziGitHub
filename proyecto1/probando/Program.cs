768ef8870efb6b6ec68d37b5272ed3132a6dea15
Miguel Santiago Gómez Suárez <Hitomiblood@gmail.com>
2025-02-02 21:08:41 -0500

    a

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Probando Commit User!");
 
-string stripeApiKey = "12345abcde67890fghij12345klmno67890pqrst";
+string stripe_Api_Key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
